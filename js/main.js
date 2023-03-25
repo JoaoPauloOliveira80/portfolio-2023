01e050ca06a2d8b9cc9b85452611798c93de868d
Dundaki <vigjoaopaulo@gmail.com>
2023-03-25 10:09:24 -0300

    grafico e card de investmeto alterado

--- js/main.js
+++ js/main.js
@@ -71,19 +71,9 @@
         data: {
             labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
             datasets: [{
-                    label: "USA",
-                    data: [15, 30, 55, 65, 60, 80, 95],
+                    label: "Proventos",
+                    data: [11.18, 37.24, 58.56],
                     backgroundColor: "rgba(235, 22, 22, .7)"
-                },
-                {
-                    label: "UK",
-                    data: [8, 35, 40, 60, 70, 55, 75],
-                    backgroundColor: "rgba(235, 22, 22, .5)"
-                },
-                {
-                    label: "AU",
-                    data: [12, 25, 45, 55, 65, 70, 60],
-                    backgroundColor: "rgba(235, 22, 22, .3)"
                 }
             ]
             },
@@ -96,18 +86,18 @@
     // Salse & Revenue Chart
     var ctx2 = $("#salse-revenue").get(0).getContext("2d");
     var myChart2 = new Chart(ctx2, {
-        type: "line",
+        type: "bar",
         data: {
             labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
             datasets: [{
-                    label: "Salse",
-                    data: [15, 30, 55, 45, 70, 65, 85],
+                    label: "Investimento",
+                    data: [4.000],
                     backgroundColor: "rgba(235, 22, 22, .7)",
                     fill: true
                 },
                 {
-                    label: "Revenue",
-                    data: [99, 135, 170, 130, 190, 180, 270],
+                    label: "Patrimonio",
+                    data: [5.000],
                     backgroundColor: "rgba(235, 22, 22, .5)",
                     fill: true
                 }
